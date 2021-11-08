f600a8817be17ba5f0bf95609d19cb0c304c26d9
MikkoMallikas85 <93676062+MikkoMallikas85@users.noreply.github.com>
2021-11-08 08:20:52 +0200

    Navi olutkauppaan

---
+++ src/Navbar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import "./App.css";
+export default function Navbar1() {
+  return (
+    <div id="navi1">
+    <Navbar expand="md">
+      <Navbar.Brand style={{ fontFamily: "Anton-regular", fontWeight: "bold", fontSize: 40, color: "#F6F6E3"}} href="#home">Pieni Olutkauppa</Navbar.Brand>
+      <Navbar.Toggle style={{color: "#F6F6E3"}} aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse style={{color: "#F6F6E3", fontSize: 40}} id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link style={{ fontFamily: "Anton-regular", color: "#F6F6E3"}} href="#home">Meistä</Nav.Link>
+          <Nav.Link style={{ fontFamily: "Anton-regular", color: "#F6F6E3"}} href="#link">Yhteystiedot</Nav.Link>
+          <NavDropdown style={{ fontFamily: "Anton-regular" }} title="Tuotekategoriat" id="collasible-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">PILSNERIT</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">LAGERIT</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">IPAT, APAT, NEIPAT</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">SOURIT</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">GOSET</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">STOUTIT, PORTTERIT</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">ALKOHOLITTOMAT</NavDropdown.Item>
+          </NavDropdown>
+          <Nav.Link style={{ fontFamily: "Anton-regular", color: "#F6F6E3", fontSize: 40 }} href="#home">Ostoskori</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+    </div>
+  );
+}
