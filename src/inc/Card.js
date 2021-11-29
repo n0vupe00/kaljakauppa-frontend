581ee5eade9cb174487cc8ea721666d183e9bc84
Erski <eero.vilkuna@gmail.com>
2021-11-29 17:52:22 +0200

    kortti ei varmasti toimi

--- src/inc/Card.js
+++ src/inc/Card.js
@@ -1,18 +1,22 @@
-import React, { useEffect, useState } from 'react'
-import { Route, Switch, useLocation } from 'react-router';
-import './App.css';
-import Navbar from './inc/Navbar';
-import Footer from './inc/Footer';
-import Home from './inc/Home';
-import Product from './Product';
+import React from "react";
+import { Button } from "react-bootstrap";
+import { Card } from "@material-ui/core";
+import Col from "react-bootstrap/Col";
 
-function Card(props){
-    return (
-        <div className = 'card'>
-            <div className='card_bodfy'>
-                <img src="kuva.jpg" class='card_image'/>
-                <h2 className="card_title">{Product.name}</h2>
-                <p className="card:description">{Product.info}</p>
-            </div>
-        </div>    )
-}
+const Product = ({ product }) => (
+  <Col xs={12} md={6} lg={4} key={product.category_id}>
+    <Card style={{ width: "18rem" }}>
+      <Card.Header></Card.Header>
+      <Card.Img variant="top" src={product.image} />
+      <Card.Body>
+        <Card.Title>{product.name}</Card.Title>
+        <Card.Text>{product.price}</Card.Text>
+        <Card.Text>{product.alcohol}</Card.Text>
+        <Card.Text>{product.size}</Card.Text>
+        <Button variant="primary">Add to cart</Button>
+      </Card.Body>
+    </Card>
+  </Col>
+);
+
+export default Product;
